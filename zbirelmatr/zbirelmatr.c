b51713a7c1db2cb5c86341ddddfa20a99f1dbed5
Dušan Simić <dusan.simic1810@gmail.com>
2016-09-23 12:20:43 +0200

    added zbirelmatr

---
+++ zbirelmatr/zbirelmatr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() {
+  int n;
+  printf("Unesite velicinu kvadratne matrice: ");
+  scanf("%i", &n);
+  int matr[n][n];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("matr[%i][%i] = ", i, j);
+      scanf("%i", &matr[i][j]);
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%3i", matr[i][j]);
+    }
+    printf("\n");
+  }
+  int suma = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (j > i)
+        suma+=matr[i][j];
+    }
+  }
+  printf("Suma brojeva iznad dijagonale je: %i\n\n", suma);
+}
