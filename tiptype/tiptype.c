6c49bfaeaf6858b5015579b71a0e5a3b92329c3c
Dušan Simić <dusan.simic1810@gmail.com>
2016-11-25 14:33:58 +0100

    dodat zadatak sa vezbi

---
+++ tiptype/tiptype.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+typedef enum Types {
+  ASC = 1,
+  DESC
+} Type;
+
+void sort(int *p, int n, Type tip) {
+  int i, j;
+  if (tip = 1) {
+    for (i = 0 ; i < ( n - 1 ); i++) {
+      for (j = 0 ; j < n - i - 1; j++) {
+        if (*(p+j) > *(p+j+1)) {
+          int swap = *(p+j);
+          *(p+j) = *(p+j+1);
+          *(p+j+1) = swap;
+        }
+      }
+    }
+  } else {
+    for (i = 0 ; i < ( n - 1 ); i++) {
+      for (j = 0 ; j < n - i - 1; j++) {
+        if (*(p+j) < *(p+j+1)) {
+          int swap = *(p+j);
+          *(p+j) = *(p+j+1);
+          *(p+j+1) = swap;
+        }
+      }
+    }
+  }
+}
+
+void init(int *p, int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    printf("Unesite %d. element niza: ", i+1);
+    scanf("%d", &(*(p+i)));
+  }
+}
+
+void print(int *p, int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    printf(" %d", *(p+i));
+  }
+  printf("\n");
+}
+
+void meni(int *p) {
+  int i;
+  do {
+    printf("1. Rastuce sortiranje\n");
+    printf("2. Opadajuce sortiranje\n");
+    printf("3. Init\n");
+    printf("4. Prikazi\n");
+    printf("5. Kraj\n");
+    scanf("%d", &i);
+  } while (i < 1 && i > 5);
+  *p = i;
+
+}
+
+void main() {
+  int fun, n;
+  printf("Unesite duzinu niza: ");
+  scanf("%d", &n);
+  int *p = malloc(sizeof(int) * n);
+  do {
+    meni(&fun);
+    switch(fun) {
+      case 1:
+        sort(p, n, fun);
+        break;
+      case 2:
+        sort(p, n, fun);
+        break;
+      case 3:
+        init(p, n);
+        break;
+      case 4:
+        print(p, n);
+        break;
+    }
+  } while (fun != 5);
+}
