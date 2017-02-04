c0de5e144daa9c5f3a4a5f558128629c90651311
Dušan Simić <dusan.simic1810@gmail.com>
2017-02-04 13:36:22 +0100

    nije gotov zadatak artikli2

--- artikli2/main.c
+++ artikli2/main.c
@@ -3,12 +3,12 @@
 
 struct artikal {
   int sifra;
-  char naziv;
+  char naziv[15];
   double cena;
 } typedef Artikal;
 
 struct prodavnica {
-  char naziv;
+  char naziv[15];
   Artikal *artikli;
   int brojArtikala;
 } typedef Prodavnica;
@@ -16,7 +16,6 @@ struct prodavnica {
 Prodavnica *p;
 
 void postaviNaziv() {
-  p -> naziv = (int*) malloc(sizeof(char) * 15);
   char tmpNaziv[15];
   gets(tmpNaziv);
   for (int i = 0; i < 15; i++) {
@@ -27,11 +26,51 @@ void postaviNaziv() {
 }
 
 void dodajArtikal() {
-  p -> brojArtikala++;
-  p -> artikli = (Artikal*) malloc(sizeof(Artikal) * brojArtikala);
-  printf("Unesite sifru: ");
-  scanf("%d", p -> (artikli + brojArtikala - 1) -> sifra);
-  
+  if (p -> brojArtikala) {
+    p -> brojArtikala++;
+    Artikal *temp = (Artikal*) malloc(sizeof(Artikal) * p -> brojArtikala);
+    *temp = *(Artikal*) p -> *artikli;
+    free(p -> artikli);
+    printf("Unesite sifru artikla: ");
+    scanf("%d", (temp + brojArtikala - 1) -> sifra);
+    printf("Unestie naziv artikla: ");
+    char tmpNaziv[15];
+    gets(tmpNaziv);
+    for (int i = 0; i < 15; i++) {
+      if (tmpNaziv[i] != 0) {
+        (temp + brojArtikala - 1) -> naziv[i] = tmpNaziv[i];
+      }
+    }
+    printf("Unesite cenu artikla: ");
+    scanf("%lf", (temp + brojArtikala - 1) -> cena);
+    p -> artikli = (Artikal*) malloc(sizeof(Artikal) * p -> brojArtikala);
+    p -> *artikli = *temp;
+    free(temp);
+  } else {
+    p -> brojArtikala++;
+    p -> artikli = (Artikal*) malloc(sizeof(Artikal));
+    printf("Unesite sifru artikla: ");
+    scanf("%d", p -> artikli -> sifra);
+    printf("Unestie naziv artikla: ");
+    char tmpNaziv[15];
+    gets(tmpNaziv);
+    for (int i = 0; i < 15; i++) {
+      if (tmpNaziv[i] != 0) {
+        p -> artikli -> naziv[i] = tmpNaziv[i];
+      }
+    }
+    printf("Unesite cenu artikla: ");
+    scanf("%lf", p -> artikli -> cena);
+  }
+}
+
+void ispisiArtikle() {
+  for (int i = 0; i < p -> brojArtikala; i++) {
+    printf("Artikal %d.\n", i+1);
+    printf("Sifra: %d\n", p -> (artikli + i) -> sifra);
+    printf("Naziv: %s\n"p -> (artikli + i) -> naziv);
+    printf("Cena: %lf\n", p -> (artikli + i) -> cena);
+  }
 }
 
 
@@ -39,6 +78,13 @@ int main() {
 
   p = (Prodavnica *p) malloc(sizeof(Prodavnica));
 
-  printf("%d", a -> sifra);
+  postaviNaziv();
+  dodajArtikal();
+  dodajArtikal();
+  ispisiArtikle();
+
+  free(p -> artikli);
+  free(p);
+
   return 0;
 }
